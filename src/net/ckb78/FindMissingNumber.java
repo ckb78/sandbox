356770566ce4df0594daf94302fef4a97c87e8c3
Christian Bjørnsrud <ckb78@outlook.com>
2021-03-20 13:18:46 +0100

    Two solutions to FindMissingNumber Puzzle.

--- src/net/ckb78/FindMissingNumber.java
+++ src/net/ckb78/FindMissingNumber.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class FindLostNumber {
+public class FindMissingNumber {
 
     public static void main(String[] args) {
 
